5eae655c597c814e7068f08748733a1a2383eb0b
boshy <kaidohmary@gmail.com>
2023-10-08 12:27:27 +0300

    fix permissions

--- habit/views.py
+++ habit/views.py
@@ -4,23 +4,21 @@ from rest_framework.permissions import IsAuthenticated
 from habit.models import Habit
 from habit.pagination import HabitPaginator
 from habit.permissions import IsOwner
+
 from habit.serializer import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated, IsOwner]
 
-    def get_permissions(self):
-        match self.action:
-            case 'update', 'partial_update', 'destroy':
-                return [IsAuthenticated(), IsOwner()]
-            case _:
-                return [IsAuthenticated()]
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
 
 
 class PublicHabitListAPIView(generics.ListAPIView):
     queryset = Habit.objects.filter(is_public=True)
     permission_classes = [IsAuthenticated]
     serializer_class = HabitSerializer
+    pagination_class = HabitPaginator
